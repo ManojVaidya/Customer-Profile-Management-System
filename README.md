3e3ec4ef662f427fcc740f5896ab2848b354a0a3
Manoj Vaidya <35463977+ManojVaidya@users.noreply.github.com>
2019-07-14 18:05:52 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Customer Profile Management System
+MEAN STack - Satellizer
+
+Run:
+-npm install
+-Update *_config.js* with the client secret
+-Update *client/app.js* with the client id
+-Run
+
+Add social
+-Add social auth logic to *server/routes/auth.js*
+-Update the user model
+-Update *_config.js* with the client secret
+-Update *client/app.js* with the client id
---
+++ client/app.js
@@ -0,0 +1,61 @@
+var app = angular.module('MyApp', ['ngRoute', 'satellizer']);
+
+app.config(function($routeProvider, $authProvider, $locationProvider) {
+
+  // *** satellizer settings *** //
+  $authProvider.github({
+    url: '/auth/github',
+    clientId: '',
+    redirectUri: window.location.origin
+  });
+  $authProvider.google({
+    url: '/auth/google',
+    clientId: '',
+    redirectUri: window.location.origin,
+  });
+  $authProvider.instagram({
+    url: '/auth/instagram',
+    clientId: '',
+    redirectUri: window.location.origin,
+  });
+
+  $routeProvider
+    .when('/', {
+      templateUrl: 'partials/welcome.html',
+      access: {restricted: false}
+    })
+    .when('/home', {
+      templateUrl: 'partials/home.html',
+      access: {restricted: false}
+    })
+    .when('/login', {
+      templateUrl: 'partials/login.html',
+      controller: 'loginCtrl',
+      access: {restricted: false}
+    })
+    .when('/signup', {
+      templateUrl: 'partials/signup.html',
+      controller: 'signupCtrl',
+      access: {restricted: false}
+    })
+    .when('/ping', {
+      template: '<h1>Pong</h1>',
+      access: {restricted: true}
+    })
+    .when('/profile', {
+      templateUrl: 'partials/profile.html',
+      controller: 'profileCtrl',
+      access: {restricted: true}
+    })
+    .otherwise('/');
+
+});
+
+app.run(function ($rootScope, $location, $route, $auth) {
+  $rootScope.$on('$routeChangeStart', function (event, next, current) {
+    if (next.access.restricted && !$auth.isAuthenticated()) {
+      $location.path('/login');
+      $route.reload();
+    }
+  });
+});
